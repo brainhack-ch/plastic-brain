1e16a81f1fd174d6e7af127db01057f64da5ac3c
Ítalo Fernandes <italogsfernandes@gmail.com>
2019-03-22 18:18:15 +0100

    updated to run also in linux

---
+++ BrainHackScripts/brainHack_player.py
@@ -0,0 +1,14 @@
+from pycnbi.stream_player.stream_player import stream_player
+import os
+
+fif_file = r'./Recordings/brainHackTestClosed.fif'
+
+if os.name == 'nt':  # nt -> Windows and posix -> Linux
+    print("Running on windows system")
+    fif_file = r'C:\Recordings\Gait\exp1 - Copy\fif\mb007_1.fif'
+
+# sample code
+if __name__ == '__main__':
+    server_name = 'StreamPlayer'
+    chunk_size = 8  # chunk streaming block size
+    stream_player(server_name, fif_file, chunk_size)
---
+++ BrainHackScripts/run_player.sh
@@ -0,0 +1,1 @@
+python brainHack_player.py
